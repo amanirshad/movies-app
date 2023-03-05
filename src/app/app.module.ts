3f46c5ccce99ae1abaee27b1b6528ab2f44b8986
amanirshad <aman.itz.me07@gmail.com>
2023-03-05 20:28:15 +0530

    Home Page Slider Banner Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,22 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { SliderComponent } from './components/slider/slider.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    MoviesComponent
+    MoviesComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
